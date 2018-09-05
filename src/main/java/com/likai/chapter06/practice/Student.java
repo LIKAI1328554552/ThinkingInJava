45bae77e02359d2a9250838f8bc18ead52eb34fa
当你途径我的盛放 <1328554552@qq.com>
2018-09-05 21:20:43 +0800

    Update Student.java

--- src/main/java/com/likai/chapter06/practice/Student.java
+++ src/main/java/com/likai/chapter06/practice/Student.java
@@ -4,10 +4,20 @@ package com.likai.chapter06.practice;
  * Created by likai on 2018/08/07.
  */
 public class Student extends Person{
+    
+    private String nubmer ;
 
     public Student() {
         Person person = new Person() ;
         person.personPrint();
+    }
+    
+    public void setNumber(String number) {
+        this.number = number ;
+    }
+    
+    public String getNumber() {
+        return this.number ;
     }
 
 }
